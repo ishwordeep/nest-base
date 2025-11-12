3a3f6f1174b3dfb775663855d1125392f50ad148
ishwordeep <ishwor055@gmail.com>
2025-11-13 00:54:05 +0545

    cart

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -8,12 +8,12 @@ export class CartController {
   constructor(private readonly cartService: CartService) {}
 
   @Post()
-  async create(@Body() createCartDto: CreateCartDto): Promise<Cart> {
+  async create(@Body() createCartDto: CreateCartDto){
     return this.cartService.create(createCartDto);
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string): Promise<Cart> {
+  async findOne(@Param('id') id: string){
     return this.cartService.findOne(id);
   }
 }
