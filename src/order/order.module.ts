90f190dcf98e4114f7b88b6552191df6d994eac6
ishwordeep <ishwor055@gmail.com>
2025-12-05 21:21:47 +0545

    update on cart delete after order

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -3,10 +3,12 @@ import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Order, OrderSchema } from './schema/create.schema';
+import { CartModule } from 'src/cart/cart.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+    CartModule,
   ],
   controllers: [OrderController],
   providers: [OrderService],
